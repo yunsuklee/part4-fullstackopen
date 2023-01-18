475cac29b039b9fbbe972aa1ccad84445b9343f1
yunsuklee <sergioleesp@gmail.com>
2023-01-18 13:34:51 +0100

    bloglist 4.4

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((likes, blog) => (
+    likes + blog.likes
+  ), 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
