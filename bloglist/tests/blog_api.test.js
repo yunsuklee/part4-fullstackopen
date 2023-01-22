b02c2ed1e1b75b48a42e0b1691d88bd893013249
yunsuklee <sergioleesp@gmail.com>
2023-01-22 19:37:54 +0100

    bloglist 4.13

--- bloglist/tests/blog_api.test.js
+++ bloglist/tests/blog_api.test.js
@@ -1,115 +1,130 @@
-const mongoose = require('mongoose')
 const supertest = require('supertest')
+const mongoose = require('mongoose')
 const helper = require('./test_helper')
 const app = require('../app')
-
 const api = supertest(app)
+
 const Blog = require('../models/blog')
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-
-  for (let blog of helper.initialBlogs) {
-    let blogObject = new Blog(blog)
-    await blogObject.save()
-  }
+  await Blog.insertMany(helper.initialBlogs)
 })
 
-/*
-  Verify that the blog list app returns the correct amount
-  of blog posts in JSON format
-*/
+describe('when there are some initial blog posts saved', () => {
+  // Test for JSON format
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  }, 10000)
 
-// Test for JSON format
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-}, 10000)
+  // Test for correct amount of blog posts
+  test('all blog posts are returned', async () => {
+    const response = await api.get('/api/blogs')
 
-// Test for correct amount of blog posts
-test('all blog posts are returned', async () => {
-  const response = await api.get('/api/blogs')
-
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
 })
 
-// Test for unique identifier id
-test('verifying existence of property id', async () => {
-  const response = await api.get('/api/blogs')
+describe('when adding new blog posts', () => {
+  // Test for unique identifier id
+  test('verifying existence of property id', async () => {
+    const response = await api.get('/api/blogs')
 
-  response.body.forEach(blog => expect(blog.id).toBeDefined())
+    response.body.forEach(blog => expect(blog.id).toBeDefined())
+  })
+
+  // Test creating a new blog
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'Adding test blog',
+      author: 'FullStackOpen',
+      url: 'https://fullstackopen.com',
+      likes: 500
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAfterAdding = await helper.blogsInDb()
+    expect(blogsAfterAdding).toHaveLength(helper.initialBlogs.length + 1)
+
+    const titles = blogsAfterAdding.map(b => b.title)
+    expect(titles).toContain('Adding test blog')
+  })
+
+  // Test missing likes property
+  test('if likes is missing it will default to 0', async () => {
+    const newBlog = {
+      title: 'No likes blog',
+      author: 'Sergio Lee',
+      url: 'https://twitter.com'
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAfterAdding = await helper.blogsInDb()
+    expect(blogsAfterAdding).toHaveLength(helper.initialBlogs.length + 1)
+
+    expect(blogsAfterAdding.at(-1).likes).toBe(0)
+  })
+
+  // Test bad request
+  test('missing url or title', async () => {
+    const noTitleBlog = {
+      author: 'Sergio Lee',
+      url: 'www.google.com',
+      likes: 1
+    }
+
+    const noUrlBlog = {
+      title: 'No url Blog',
+      author: 'Sergio Lee',
+      likes: 10
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(noTitleBlog)
+      .expect(400)
+
+    await api
+      .post('/api/blogs')
+      .send(noUrlBlog)
+      .expect(400)
+
+    const blogsAfterAdding = await helper.blogsInDb()
+    expect(blogsAfterAdding).toHaveLength(helper.initialBlogs.length)
+  })
 })
 
-// Test creating a new blog
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'Adding test blog',
-    author: 'FullStackOpen',
-    url: 'https://fullstackopen.com',
-    likes: 500
-  }
+describe('manipulating already existing blog posts', () => {
+  // Test for deleting a single blog post
+  test('deleting a single blog post', async () => {
+    const blogsBeforeDeletion = await helper.blogsInDb()
+    const blogToDelete = blogsBeforeDeletion[0]
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
 
-  const blogsAfterAdding = await helper.blogsInDb()
-  expect(blogsAfterAdding).toHaveLength(helper.initialBlogs.length + 1)
+    const blogsAfterDeletion = await helper.blogsInDb()
+    expect(blogsAfterDeletion).toHaveLength(helper.initialBlogs.length - 1)
+  })
 
-  const titles = blogsAfterAdding.map(b => b.title)
-  expect(titles).toContain('Adding test blog')
-})
+  // Test for updating a blog post
+  test('updating blog post information', async () => {
 
-// Test missing likes property
-test('if likes is missing it will default to 0', async () => {
-  const newBlog = {
-    title: 'No likes blog',
-    author: 'Sergio Lee',
-    url: 'https://twitter.com'
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAfterAdding = await helper.blogsInDb()
-  expect(blogsAfterAdding).toHaveLength(helper.initialBlogs.length + 1)
-
-  expect(blogsAfterAdding.at(-1).likes).toBe(0)
-})
-
-// Test bad request
-test('missing url or title', async () => {
-  const noTitleBlog = {
-    author: 'Sergio Lee',
-    url: 'www.google.com',
-    likes: 1
-  }
-
-  const noUrlBlog = {
-    title: 'No url Blog',
-    author: 'Sergio Lee',
-    likes: 10
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(noTitleBlog)
-    .expect(400)
-
-  await api
-    .post('/api/blogs')
-    .send(noUrlBlog)
-    .expect(400)
-
-  const blogsAfterAdding = await helper.blogsInDb()
-  expect(blogsAfterAdding).toHaveLength(helper.initialBlogs.length)
+  })
 })
 
 afterAll(() => {
--- bloglist/tests/test_helper.js
+++ bloglist/tests/test_helper.js
@@ -2,61 +2,53 @@ const Blog = require('../models/blog')
 
 const initialBlogs = [
   {
-    id: '5a422aa71b54a676234d17f8',
     title: 'Go To Statement Considered Harmful',
     author: 'Edsger W. Dijkstra',
     url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
     likes: 5
   },
   {
-    id: '5a422aa71b54a676234d17f8',
     title: 'Go To Statement Considered Harmful',
     author: 'Edsger W. Dijkstra',
     url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
     likes: 5
   },
   {
-    id: '5a422aa71b54a676234d17f8',
     title: 'Go To Statement Considered Harmful',
     author: 'Edsger W. Dijkstra',
     url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
     likes: 5
   },
   {
-    id: '5a422aa71b54a676234d17f8',
     title: 'Go To Statement Considered Harmful',
     author: 'Sergio Lee',
     url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
     likes: 5
   },
   {
-    id: '5a422aa71b54a676234d17f8',
     title: 'Go To Statement Considered Harmful',
     author: 'Coral Neira',
     url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
     likes: 5
   },
   {
-    id: '5a422aa71b54a676234d17f8',
     title: 'Go To Statement Considered Harmful',
     author: 'Coral Neira',
     url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
     likes: 5
   },
   {
-    id: '5a422aa71b54a676234d17f8',
     title: 'Go To Statement Considered Harmful',
     author: 'Coral Neira',
     url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
     likes: 5
   },
   {
-    id: '5a422aa71b54a676234d17f8',
     title: 'Go To Statement Considered Harmful',
     author: 'Coral Neira',
     url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
     likes: 5
-  },
+  }
 ]
 
 const blogsInDb = async () => {
