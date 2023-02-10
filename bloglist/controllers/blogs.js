83cc35954dd7434e7a5b5201fc984b239cc4f219
yunsuklee <sergioleesp@gmail.com>
2023-02-10 11:54:31 +0100

    bloglist 4.21

--- bloglist/controllers/blogs.js
+++ bloglist/controllers/blogs.js
@@ -42,7 +42,19 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id)
+  const blog = await Blog.findById(request.params.id)
+  // eslint-disable-next-line no-undef
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'invalid token' })
+  }
+  const user = await User.findById(decodedToken.id)
+
+  if (blog.user.toString() === user.id) {
+    await Blog.findByIdAndRemove(blog.id)
+  } else{
+    return response.status(401).json({ error: 'invalid token' })
+  }
   response.status(204).end()
 })
 
