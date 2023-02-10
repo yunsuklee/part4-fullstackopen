4844cd109a3165491a687b8905950c727d08a0d7
yunsuklee <sergioleesp@gmail.com>
2023-02-10 11:05:52 +0100

    bloglist 4.20

--- bloglist/controllers/blogs.js
+++ bloglist/controllers/blogs.js
@@ -19,18 +19,10 @@ blogsRouter.get('/:id', async (request, response) => {
   }
 })
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
-
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
   // eslint-disable-next-line no-undef
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token invalid' })
   }
